e9a4b85942faf0ab66a3d487be3902b2111d9983
venu-shetty <venushetty24@gmail.com>
2026-03-11 14:09:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,23 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Cart from "./components/Cart";
 import Orders from "./components/Orders";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header />
 
-      <Content />
-
-      <Login />
-
-      <Register />
-
-      <Cart />
-
-      <Orders />
-
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Content />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
